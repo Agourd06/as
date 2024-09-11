73adf5f4fc7fac899f54832dd48933bba13840b3
Agourd06 <oualidagourd@gmail.com>
2024-09-11 17:08:54 +0100

    FEAT:change strucer of code by sperate model logique and controller logique to make the code more structerd

--- models/formateurModel.js
+++ models/formateurModel.js
@@ -1,42 +1,126 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const db = require("../config/database");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-const formateur = sequelize.define('formateur', {
+const FormateurModel = {
 
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    prenom: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+  createFormateur: async (formateurData) => {
+    const {
+      name,
+      prenom,
+      birth,
+      adress,
+      specialite,
+      email,
+      password
+    } = formateurData;
 
-    birth: {
-        type: DataTypes.DATE,
-        allowNull: false,
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const insertFormateurQuery = `
+      INSERT INTO formateur (name, prenom, birth, adress, specialite, email, password)
+      VALUES (?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    return new Promise((resolve, reject) => {
+      db.query(
+        insertFormateurQuery,
+        [name, prenom, birth, adress, specialite, email, hashedPassword],
+        (err, result) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(result.insertId);
+        }
+      );
+    });
+  },
+
+  checkEmailExists: (email) => {
+    const checkEmailQuery = 'SELECT * FROM formateur WHERE email = ?';
+
+    return new Promise((resolve, reject) => {
+      db.query(checkEmailQuery, [email], (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results.length > 0);
+      });
+    });
+  },
+
+  assignClass: (formateurId) => {
+    const formateurClassQuery = `
+      INSERT INTO class (formateur_id)
+      VALUES (?)
+    `;
+
+    return new Promise((resolve, reject) => {
+      db.query(formateurClassQuery, [formateurId], (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result);
+      });
+    });
+  },
+
+
+
+
+    updateFormateur: (id, formateurData) => {
+      const {
+        name,
+        prenom,
+        birth,
+        adress,
+        specialite
+      } = formateurData;
+  
+      const sqlQuery = `
+        UPDATE formateur 
+        SET name = ?, prenom = ?, birth = ?, adress = ?, specialite = ?
+        WHERE id = ?
+      `;
+  
+      return new Promise((resolve, reject) => {
+        db.query(
+          sqlQuery,
+          [name, prenom, birth, adress, specialite, id],
+          (err, result) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(result);
+          }
+        );
+      });
     },
-    adress: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    specialite: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+  
+    getFormateurById: (id) => {
+      const sqlQuery = 'SELECT * FROM formateur WHERE id = ?';
+  
+      return new Promise((resolve, reject) => {
+        db.query(sqlQuery, [id], (err, results) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(results.length > 0 ? results[0] : null);
+        });
+      });
     }
-    
-}, {
-    tableName: 'formateur',
-    timestamps: false
-});
-
-
-module.exports = formateur;
+    ,
+    deleteFormateur : (id) => {
+        return new Promise((resolve, reject) => {
+          const sql = "UPDATE formateur SET deleted_at = NOW() WHERE id = ?";
+          db.query(sql, [id], (err, results) => {
+            if (err) return reject(err);
+            else resolve(results)
+          });
+        })
+      },
+     
+      
+  };
+  
+module.exports = FormateurModel;
--- routes/etudiantRoutes.js
+++ routes/etudiantRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const etudiantController = require('../controllers/etudiantController');
 const checkRole = require('../middleware/checkRole');
 
-router.get('/',checkRole('etudiant'), etudiantController.getAllStudents);
+// router.get('/',checkRole('etudiant'), etudiantController.getAllStudents);
 
 router.get('/home',checkRole('etudiant'), (req, res) => {
     res.render('etudiant/etudiant');  
